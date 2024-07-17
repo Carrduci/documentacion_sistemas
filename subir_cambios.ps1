a2aa49525e0982ec4a775846de573d53f62cf83f
CarrducistemasTI <sistemas.ti@carrduci.com>
2024-07-17 10:58:49 -0600

    feat: cambio a documentacion (#3)
    M  subir_cambios.ps1

--- subir_cambios.ps1
+++ subir_cambios.ps1
@@ -1,4 +1,7 @@
+$mensajeExtra = $args[0]
+$tipoCommit = $args[1]
+
 git add *
 $Mensaje = git status -s -v
-git commit -m "cambio a documentacion" -m "${Mensaje}"
+git commit -m "${tipoCommit}: cambio a documentacion${mensajeExtra}" -m "${Mensaje}"
 git push --follow-tags origin main
